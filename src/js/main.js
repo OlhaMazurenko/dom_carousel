4c1119a66bf1cbe99709ee33dc082bf878069b10
OlhaMazurenko <mail.olgamazurenko@gmail.com>
2019-05-29 09:25:30 +0300

    'Solution'

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,27 @@
 let arrowLeft = document.getElementById('previous');
 let arrowRight = document.getElementById('next');
-let element = document.getElementsByClassName('carusel_slides')[0];
-let counter =260;
-let initialValue = 260;
-let moveRight =  element.style.right = counter + 'px';
+let element = document.querySelector('.carusel_slides');
+let counter = 0;
+let imgWidth = document.querySelector('img').offsetWidth;
+let initialValue = 390;
 
 arrowRight.addEventListener('click', ()=> {
   counter += initialValue;
   arrowRight.disabled = false;
+   if (counter > 780) {
+    arrowRight.disabled = true;
+   }
   element.style.right = counter + 'px';
-  if (counter > 780) arrowRight.disabled = true;
   arrowLeft.disabled = false;
 });
 
 arrowLeft.addEventListener('click', ()=> {
-  arrowLeft.disabled = true;
   arrowRight.disabled = false;
   counter -= initialValue;
   arrowLeft.disabled = false;
-  element.style.right = (counter - initialValue) + 'px';
-  if (parseInt(getComputedStyle(element).right) <= 260) arrowLeft.disabled = true;
- }); 
+  if (counter <= 0) {
+    counter = 0;
+    arrowLeft.disabled = true;
+  }
+  element.style.right = counter + 'px';
+}); 
